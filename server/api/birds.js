ee06ab8c8e0948900c58d257e243dc817675209a
Grace Gao <gracegao@Graces-MacBook-Pro.local>
2023-04-06 14:43:21 -0400

    able to add new birds

--- server/api/birds.js
+++ server/api/birds.js
@@ -4,9 +4,27 @@ module.exports = router
 //api/birds in postman 
 router.get('/', async (req, res, next) => {
   try {
-    console.log("working")
     const birds = await Birds.findAll()
     res.json(birds)
+  } catch (err) {
+    next(err)
+  }
+})
+router.get('/:id', async (req, res, next) => {
+  try {
+    const {id}=req.params
+    res.json(await Birds.findAll( {
+
+        where: { id: id }
+    }))
+  } catch (err) {
+    next(err)
+  }
+})
+router.post('/', async (req, res, next) => {
+  try {
+    const newBird = await Birds.create(req.body)
+    res.json(newBird)
   } catch (err) {
     next(err)
   }
